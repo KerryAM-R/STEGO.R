f672aeafe4e6c3921c05eb0dbd285d4c01dc0c64
KerryAM-R <83262343+KerryAM-R@users.noreply.github.com>
2023-12-13 09:29:44 +0100

    update
    remove clusTCR2 and motifStack

--- R/app.R
+++ R/app.R
@@ -8,7 +8,61 @@
 
 runSTEGO <- function(){
 
-  # source(system.file("Global","required_functions.R",package = "STEGO.R"))
+  suppressWarnings(require("DescTools"))
+  suppressMessages(require("bslib"))
+  suppressMessages(require("circlize")) # colorRamp2
+  # suppressMessages(suppressWarnings(require("ClusTCR2")))
+  suppressMessages(require("colourpicker")) # select visual colour
+  suppressMessages(require("ComplexHeatmap"))
+  suppressMessages(require("corrplot"))
+  suppressMessages(require("doParallel"))
+  suppressMessages(require("dplyr"))
+  suppressMessages(require("dior"))
+  suppressMessages(require("DT"))
+  suppressMessages(require("forcats"))
+  suppressMessages(require("foreach"))
+  suppressMessages(require("fpc")) #
+  suppressMessages(require("GGally"))
+  suppressMessages(require("ggnet"))
+  suppressMessages(require("ggpattern"))
+  suppressMessages(require("ggplot2"))
+  suppressMessages(require("ggrepel"))
+  suppressMessages(require("ggridges")) # ridges plot
+  suppressMessages(require("ggseqlogo")) # create logo figure
+  suppressMessages(require("gridExtra"))
+  suppressMessages(require("harmony"))
+  suppressMessages(require("igraph"))
+  suppressMessages(require("iterators"))
+  suppressMessages(require("linkcomm")) # create the network graphs.
+  suppressMessages(require("lubridate")) # create the network graphs.
+  suppressMessages(require("Matrix"))
+  # suppressMessages(require("motifStack")) # function
+  suppressMessages(require("network"))
+  suppressMessages(require("plyr"))
+  suppressMessages(require("purrr"))
+  suppressMessages(require("randomcoloR"))
+  suppressMessages(require("RColorBrewer"))
+  suppressMessages(require("Rcpp"))
+  suppressMessages(require("readr"))
+  suppressMessages(require("reshape2")) # acast function
+  suppressMessages(require("scGate"))
+  suppressMessages(require("Seurat"))
+  suppressMessages(require("SeuratObject"))
+  suppressMessages(require("SeuratData"))
+  suppressMessages(require("SeuratDisk"))
+  suppressMessages(require("shiny"))
+  suppressMessages(require("shinyBS"))
+  suppressMessages(require("shinybusy"))
+  suppressMessages(require("shinyWidgets"))
+  suppressMessages(require("showtext"))
+  suppressMessages(require("showtextdb"))
+  suppressMessages(require("stringr"))
+  suppressMessages(require("sysfonts"))
+  suppressMessages(require("tibble"))
+  suppressMessages(require("tidyr"))
+  suppressMessages(require("VLF"))
+  require("chisq.posthoc.test")
+
   suppressWarnings(source(system.file("scGATE","custom_df_scGATE.R",package = "STEGO.R")))
   suppressWarnings(source(system.file("scGATE","custom_annotation_models.R",package = "STEGO.R")))
 
